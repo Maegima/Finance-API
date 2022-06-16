f78bec93780d84638261eacfa3d75c90543a574e
maegima <andre.maegima@unifesp.br>
2022-06-16 17:09:08 -0300

    Added cors

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { FinanceRouter, AccountRouter, HistoryRouter } from 'app/router';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express()
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use([FinanceRouter, AccountRouter, HistoryRouter]);
 
